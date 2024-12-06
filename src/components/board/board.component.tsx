7bcbca7307769f1d93c23609e222a1780844537d
puzika <tolibjonlatipovk@gmail.com>
2024-12-06 18:38:10 +0700

    implemented piece positioning depending on player color

--- src/components/board/board.component.tsx
+++ src/components/board/board.component.tsx
@@ -1,21 +1,34 @@
-import { RANKS, FILES } from './board.slice';
+import { useAppSelector } from '../../store/hooks';
+import { selectPlayer } from '../game/game.slice';
+import { RANKS, FILES, selectPosition } from './board.slice';
 import { getCellColor } from './board.utils';
+import { generateBoardFromFen } from './board.utils';
+import { pieces } from '../../pieces/pieces.images';
+import type { Color } from './board.slice';
 import * as S from './board.style';
 import * as svar from '../../variables.style';
 
 export default function Board() {
+   const player: Color = useAppSelector(selectPlayer);
+   const position: string = useAppSelector(selectPosition);
 
+   const ranks: string[] = player === 'w' ? [...RANKS].reverse() : RANKS;
+   const files: string[] = player === 'w' ? FILES : [...FILES].reverse();
+   const board: string[][] = player === 'w' ? generateBoardFromFen(position) : generateBoardFromFen(position.split('').reverse().join(''));
 
    return (
       <S.Board>
          {
-            RANKS.map((rank, idxRank) => (
-               FILES.map((file, idxFile) => (
+            ranks.map((rank, idxRank) => (
+               files.map((file, idxFile) => (
                   <S.Cell 
-                     key={rank + file} 
+                     key={file + rank} 
                      $backgroundColor={getCellColor(idxRank, idxFile) === 'w' ? svar.clrCellWhite : svar.clrCellBlack} 
-                     $color={getCellColor(idxRank, idxFile) === 'w' ? svar.clrCellBlack : svar.clrCellWhite}>
-
+                     $color={getCellColor(idxRank, idxFile) === 'w' ? svar.clrCellBlack : svar.clrCellWhite}
+                  >
+                     { idxRank === ranks.length - 1 && <S.FileMark>{file}</S.FileMark> }
+                     { idxFile === files.length - 1 && <S.RankMark>{rank}</S.RankMark> }
+                     { !!board[idxRank][idxFile] && <S.Piece src={pieces[board[idxRank][idxFile]]} /> }
                   </S.Cell>
                ))
             ))
--- src/components/board/board.utils.ts
+++ src/components/board/board.utils.ts
@@ -3,3 +3,24 @@ import type { Color } from "./board.slice";
 export function getCellColor(row: number, col: number): Color {
    return (row % 2 === 0 && col % 2 === 0) || (row % 2 !== 0 && col % 2 !== 0) ? 'w' : 'b';
 }
+
+export function generateBoardFromFen(fen: string): string[][] {
+   const board: string[][] = [];
+   const fenRows: string[] = fen.split('/');
+
+   for (const row of fenRows) {
+      const boardRow: string[] = [];
+
+      for (const char of row) {
+         if (isFinite(Number(char))) {
+            for (let i = 0; i < Number(char); i++) boardRow.push('');
+         } else {
+            boardRow.push(char);
+         }
+      }
+
+      board.push(boardRow);
+   }
+
+   return board;
+}
