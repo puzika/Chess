67aee5a22c9a40e2b4ab5f679792bf24f2dd4f96
puzika <tolibjonlatipovk@gmail.com>
2025-01-11 19:04:46 +0700

    turned best move bar into best move button

--- src/components/best-move/best-move.component.tsx
+++ src/components/best-move/best-move.component.tsx
@@ -2,8 +2,6 @@ import * as S from './best-move.style';
 
 export default function BestMove() {
    return (
-      <S.BestMoveContainer>
-         <S.BestMove>Best move: e3</S.BestMove>
-      </S.BestMoveContainer>
+      <S.BestMove>Best move: e3</S.BestMove>
    )
 }
--- src/components/best-move/best-move.style.tsx
+++ src/components/best-move/best-move.style.tsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import * as svar from '../../variables.style';
 
-export const BestMoveContainer = styled.div`
-   background-color: ${svar.clrPrimary};
-   padding: 1rem 0;
-   border-radius: inherit;
-`;
-
-export const BestMove = styled.p`
+export const BestMove = styled.button`
+   color: inherit;
    font-size: 2.5rem;
-   text-align: center;
+   padding: 1rem 0;
+   background-color: ${svar.clrPrimary};
+   border: none;
+   border-radius: inherit;
+   transition: background-color .2s;
+   cursor: pointer;
+
+   &:hover {
+      background-color: ${svar.clrSecondary};
+   }
 `;
