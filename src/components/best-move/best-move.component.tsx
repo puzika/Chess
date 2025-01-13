076512e18f43c066c86f2b2cd116e40750b4b0dd
puzika <tolibjonlatipovk@gmail.com>
2025-01-13 14:58:59 +0700

    implemented fetching best move for analysis

--- src/components/best-move/best-move.component.tsx
+++ src/components/best-move/best-move.component.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { selectCurrPosition, selectBestMove, selectError, selectLoading } from '../../routes/analysis-route/analysis-route.slice';
+import { fetchBestMove } from '../../routes/analysis-route/analysis-route.slice';
 import * as S from './best-move.style';
 
 export default function BestMove() {
+   const dispatch = useAppDispatch();
+   const currPosition = useAppSelector(selectCurrPosition);
+   const loading = useAppSelector(selectLoading);
+   const error = useAppSelector(selectError);
+   const bestMove = useAppSelector(selectBestMove);
+
+   const handleClick = async () => {
+      try {
+         const response = await dispatch(fetchBestMove(currPosition)).unwrap();
+      } catch(err) {
+         console.log(err);
+      }
+   }
+
    return (
-      <S.BestMove>Best move: e3</S.BestMove>
+      <S.BestMove onClick={handleClick}>Best move: {bestMove}</S.BestMove>
    )
 }
