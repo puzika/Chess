12998283d8d3ba6eaa02a03f1b827be34caac29e
puzika <tolibjonlatipovk@gmail.com>
2025-01-27 22:25:58 +0700

    prevented from requesting best move when game is finished

--- src/components/best-move/best-move.component.tsx
+++ src/components/best-move/best-move.component.tsx
@@ -3,20 +3,24 @@ import { selectBestMove } from '../../utils/stockfish';
 import { selectCurrPosition } from '../../routes/analysis-route/analysis-route.slice';
 import { fetchBestMove } from '../../utils/stockfish';
 import { selectLoading } from '../../utils/stockfish';
+import { selectGameState } from '../game/game.slice';
 import Loading from '../loading/loading.component';
-import type { Response, RequestState } from '../../utils/stockfish';
+import type { GameState } from '../game/game.slice';
+import type { RequestState } from '../../utils/stockfish';
 import * as S from './best-move.style';
 
 export default function BestMove() {
    const dispatch = useAppDispatch();
+   const gameState: GameState = useAppSelector(selectGameState);
    const currPosition: string = useAppSelector(selectCurrPosition);
    const bestMove: string = useAppSelector(selectBestMove);
    const requestState: RequestState = useAppSelector(selectLoading);
 
    const handleClick = async () => {
+      if (gameState === 'FINISHED') return;
+
       try {
-         const response: Response = await dispatch(fetchBestMove(currPosition)).unwrap();
-         console.log(response);
+         await dispatch(fetchBestMove(currPosition)).unwrap();
       } catch(err) {
          alert(err);
       }
